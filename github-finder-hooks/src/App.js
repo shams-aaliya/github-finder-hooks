93da0fc5412c3c783c3051ee2e5406262b73dbf1
shams-aaliya <shamsaaliya08@gmail.com>
2021-08-31 16:42:13 +0530

    Created 2 new components Home and NotFound. Cleared App JS clutter

--- github-finder-hooks/src/App.js
+++ github-finder-hooks/src/App.js
@@ -1,12 +1,13 @@
-import React,{Fragment} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar.jsx';
+import Home from './components/pages/Home'
+import NotFound from './components/pages/NotFound';
 import About from './components/pages/About'
 import Alert from './components/layout/Alert'
-import Search from './components/users/Search';
-import Users from './components/users/Users.jsx';
 import User from './components/users/User';
+
 
 import AlertState from './context/alert/AlertState'
 import GithubState from './context/github/GithubState'
@@ -22,15 +23,10 @@ return (
   <div className="container">
   <Alert/>
       <Switch>
-      <Route exact path='/' render={props => (
-        <Fragment>
-        <Search/>
-        <Users />
-        </Fragment>
-        )}/>
+      <Route exact path='/' component={Home}/>
         <Route exact path='/about' component={About}/>
-        <Route exact path='/user/:login' 
-        component={User} />
+        <Route exact path='/user/:login' component={User} />
+        <Route component={NotFound}/>
         </Switch>
       </div>
       </div>
